52dc9dd59c97583d5eb475f099004c30192037b3
Gitana Smatauskaite <gitana.smat@gmail.com>
2023-06-12 02:03:43 +0300

    css media korekcija

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -8,13 +8,13 @@ const Country = ( { allCountries, getOneCountryInfo }) => {
     <div className = "d-flex justify-content-evenly flex-wrap" style={{fontFamily: 'Raleway'}}>
         {allCountries !== undefined && allCountries.length !== 0 ?  
         allCountries.map((country, index) => (
-            <Card style = {{ width: '20rem', margin: '1rem', border: '0.5px rgba(0, 0, 0, 0.493) solid', backgroundColor: 'rgba(0, 0, 0, 0.030)' }} key={index}>
+            <Card style = {{ width: '20rem', margin: '1rem', border: '0.5px rgba(0, 0, 0, 0.493) solid'}} key={index}>
                 <Card.Text className="d-flex align-self-end" style={{margin: '0.5rem'}}>{country.region}</Card.Text>
-                <Card.Text className="d-flex align-self-end" style={{margin: '0.5rem'}}>Capital: {country.capital}</Card.Text>
-                <Card.Title className="d-flex align-self-center">{country.name.common}</Card.Title>
+                <Card.Text className="d-flex align-self-end" style={{fontWeight:'bold', margin: '0.5rem'}}>{country.capital}</Card.Text>
+                <Card.Title className="d-flex align-self-center" style={{fontWeight:'bold'}}>{country.name.common}</Card.Title>
                 <Card.Body className="d-flex flex-wrap">
                 <Card.Img style={{height: '10rem', marginBottom: '1rem', border: '0.5px rgba(0, 0, 0, 0.493) solid'}} variant = "top" src={country.flags.png} />    
-                <Button className="d-flex float-right" variant="secondary" onClick={() => getOneCountryInfo(country.name.common) }> More info</Button>
+                <Button className="d-flex btn btn-block col-12 justify-content-center" variant="dark" onClick={() => getOneCountryInfo(country.name.common) }> More info</Button>
                 </Card.Body>
             </Card>
         ))
--- src/components/Regions.jsx
+++ src/components/Regions.jsx
@@ -1,17 +1,26 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const Regions = ({ regions, filterData, searchCountries }) => {
-    console.log(regions);
+  console.log(regions)
 
-return (
+  return (
     <div>
-<Form > 
+      <Navbar className="md-3" bg="light" variant="light">
+        <Navbar.Brand className="ms-auto" href="#home">
+          Countries
+        </Navbar.Brand>
+        <Nav className="ms-auto d-flex flex-wrap">
+          {regions.map((region, index) => (
+            <Nav.Link href="#" key={index} onClick={() => filterData(region)}>
+              {region}
+            </Nav.Link>
+          ))}
+          <Form>
             <Form.Control
               type="search"
               placeholder="Search Country..."
@@ -19,20 +28,10 @@ return (
               aria-label="Search"
               onChange={(e) => searchCountries(e.target.value)}
             />
-            </Form>
-        <Navbar bg="light" variant="light">
-        
-
-            <Navbar.Brand  className="ms-auto" href = "#home">Countries</Navbar.Brand>
-            <Nav className = "me-auto d-flex flex-wrap">
-                { regions.map((region, index) => (
-                    <Nav.Link href ="#" key={index} onClick={() => filterData(region)}>{region}</Nav.Link>
-                ))}
-
-            </Nav>
-            
-        </Navbar>
+          </Form>
+        </Nav>
+      </Navbar>
     </div>
-)
+  )
 }
 export default Regions
